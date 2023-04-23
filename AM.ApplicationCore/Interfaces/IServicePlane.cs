7b750c40735ce7809332f115b8526b36faff9c0b
mohamedkhames <mohamedkhames.mhadhbi@esprit.tn>
2023-04-23 18:33:24 +0100

    tp7

---
+++ AM.ApplicationCore/Interfaces/IServicePlane.cs
@@ -0,0 +1,18 @@
+﻿using AM.ApplicationCore.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AM.ApplicationCore.Interfaces
+{
+    public interface IServicePlane: IService<Plane>
+    {
+        public IList<Passenger> GetPassengers(Plane p);
+        public IList<Flight> GetFlights(int n);
+        public bool IsAvailablePlane(Flight f, int n);
+        public void DeletePlanes();
+    }
+    
+}
